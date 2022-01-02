364e79f9e2ab2c7869f8ede38c0b32451a8455d0
awleklin <alexwleklinski@gmail.com>
2022-01-01 20:38:17 -0600

    Initial Commit

---
+++ Tester App/Tester_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tester_AppApp.swift
+//  Tester App
+//
+//  Created by Alex Wleklinski on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Tester_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
